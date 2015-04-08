e2f505f5a7c0465e98720311fd2770b295951ae5
Tulio <tulio@doubleleft.com>
2015-04-08 15:14:12 -0300

    Added input page

--- app/views.py
+++ app/views.py
@@ -1,13 +1,21 @@
 from app import app
 from flask import render_template
-from . import wikipedia
 from flask import Markup
+from flask import request
+from . import wikipedia
 
 @app.route('/')
 @app.route('/index')
 def index():
+  return render_template('index.html')
 
-  page = wikipedia.page('São Paulo')
+@app.route('/presentation', methods=['POST'])
+def presentation():
+
+  subject = request.form['subject']
+  print(subject)
+
+  page = wikipedia.page(subject)
   sections = page.sections
 
   sections_html = "<ul>"
@@ -16,7 +24,7 @@ def index():
     sections_html = sections_html + "<li>" + item + "</li>"
   sections_html = sections_html + "</ul>"
 
-  return render_template('index.html',
+  return render_template('presentation.html',
     title=page.title,
     summary=page.summary.split('.')[0],
     sections=Markup(sections_html))
