1c88aade8add5977db02ae7835b4a99f121a5f75
Tulio <tulio@doubleleft.com>
2015-04-07 21:27:11 -0300

    General sections structure

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from . import wikipedia
+from flask import Markup
 
 @app.route('/')
 @app.route('/index')
@@ -11,10 +12,11 @@ def index():
 
   sections_html = "<ul>"
   for item in sections:
+    print(item)
     sections_html = sections_html + "<li>" + item + "</li>"
   sections_html = sections_html + "</ul>"
 
   return render_template('index.html',
     title=page.title,
     summary=page.summary.split('.')[0],
-    sections=sections)
+    sections=Markup(sections_html))
