1aa1094a4baf4421d7140d81689d66387229f1f1
Lucas <lucasdnd@gmail.com>
2015-04-14 12:37:11 -0300

    Removed heroku stuff

--- run.py
+++ run.py
@@ -20,4 +20,3 @@ with open("app.conf") as conf:
     print("Logging is DISABLED by default.\nTo enable it, edit 'app.conf', set 'logging' to 'True' and enter your database connection information as needed.\n")
 
 # app.run(debug=True, host='0.0.0.0', port=8000)
-app.run()
