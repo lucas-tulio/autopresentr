62981623fb3092adbf725303d935de4164d6c257
Tulio <tulio@doubleleft.com>
2015-05-18 22:17:36 -0300

    Created cache tables

--- db/create-schema.sql
+++ db/create-schema.sql
@@ -11,3 +11,19 @@ create table logs (
   is_random boolean default 0,
   created_at timestamp default current_timestamp
 ) engine=innodb charset=utf8;
+
+create table languages (
+  id int(11) primary key not null auto_increment,
+  language varchar(255)
+) engine=innodb charset=utf8;
+
+create table cache (
+  id int(11) primary key not null auto_increment,
+  language_id int(11) not null,
+  title varchar(255),
+  content text,
+  html text,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp,
+  foreign key (language_id) references languages(id)
+) engine=innodb charset=utf8;
